d520afc8b321e8f284427e8fcd396f65a9d20f40
Ben Kollar <benjkollar03@gmail.com>
2024-12-04 18:00:24 +0000

    day 3 part 2

--- day3/bin/main.ml
+++ day3/bin/main.ml
@@ -1,16 +1,14 @@
+
+
 open Printf
 open Shared
-
-(**)
 let mul_matcher= "mul\\(\\d+,\\d+\\)"
 let mul_enable = "do\\(\\)"
 let mul_disable= "don't\\(\\)"
 
 let expr = file_as_string "input";;
-let mults = Pcre.extract_all ~rex: (Pcre.regexp mul_matcher) expr;;
-
-
-Array.iter (fun i -> printf "%s \n" (Array.get i 0) ) mults;;
+let mults expr = let m = Pcre.extract_all ~rex: (Pcre.regexp mul_matcher) expr in
+  Array.map (fun i -> Array.get i 0) m;;
 
 let mul : string -> int = fun expr ->
     let vals = Pcre.extract_all ~rex: (Pcre.regexp "\\d+") expr in
@@ -20,7 +18,7 @@ let mul : string -> int = fun expr ->
 ;;
 
 
-printf "part 1: %d \n" ( Array.fold_left (fun acc i -> acc + mul (Array.get i 0) ) 0 mults );;
+printf "part 1: %d \n" ( Array.fold_left (fun acc i -> acc + mul i ) 0 (mults expr) );;
 (*
   let _ = Array.fold_left (fun acc i -> acc + (mul 1) ) expr 0;;
 *)  
@@ -31,9 +29,14 @@ let _print_string_array_array arr =
     ) arr
   
 
-let expr = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";;
-let expr= "do()" ^ expr;;
-print_endline expr
-let mult_enabled_blocks = Pcre.extract_all ~rex: (Pcre.regexp (sprintf "(%s.*?(?=%s).*?(?=%s)*)" mul_enable mul_matcher mul_disable)) expr;;
+let _expr = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";;
+let expr= "do()" ^ expr ^ "don't()";;
+print_endline ("expr: "^expr);;
+let mult_enabled_blocks = Pcre.extract_all ~rex: (Pcre.regexp (sprintf "(?:%s)(?:.|\\s)*?(?:%s)" mul_enable mul_disable)) expr;; 
+(* still end up capturing dos/donts as the group that is the whole expression*)
 
-Array.iter()
+Array.iter (fun i -> (Array.iter (fun j -> printf "%s ||" j) i); print_newline ()) mult_enabled_blocks;;
+
+let mult_enabled = Array.fold_left (fun acc s -> acc^s ) "" Array.(concat (to_list mult_enabled_blocks)) ;;
+
+printf "part 2: %d \n" (Array.fold_left (fun acc i -> acc+ mul i ) 0 (mults mult_enabled))
