68ec391f192a222dc3199a4a2720bd7a44f225b5
Ben Kollar <benjkollar03@gmail.com>
2024-12-06 00:26:11 +0000

    starting day 5 part 2

--- day5/bin/main.ml
+++ day5/bin/main.ml
@@ -7,7 +7,7 @@ print_newline();;
 print_endline "input";;
 List.iter (fun i -> print_endline i) input;;
 
-let (rules,pages) = 
+let (rules,updates) = 
   let rec aux r p =
     match p with
     | [] -> failwith "EOF before recieved pages itinary"
@@ -20,7 +20,7 @@ List.iter (fun i -> print_endline i) rules;;
 
 print_newline();;
 print_endline "pages";;
-List.iter (fun i -> print_endline i) pages;;
+List.iter (fun i -> print_endline i) updates;;
 
 () ;;
 
@@ -31,11 +31,42 @@ let string_to_rule str = let x = (String.split_on_char '|' str) in match x with
   | _ -> failwith "failed to parse rule" 
 ;;
 
-let _ = List.fold_left (fun acc pagestr -> let page = (String.split_on_char ',' pagestr) in 
-  if (List.fold_left (fun acc rule -> acc && (
-    let (lp,rp) = string_to_rule rule in 
-      not (List.exists (fun x -> x=lp) page) || not (List.exists (fun x -> x=rp) page) || (*if pages of the rule dont exist then dont check*)
+
+let part1 update acc (lp,rp) =  acc && (
       (*check rhat left page occurs before right page*)
-      ( List.find_index (fun x -> x=lp) page < List.find_index (fun x -> x=rp) page)
+      ( List.find_index (fun x -> x=lp) update < List.find_index (fun x -> x=rp) update)
       (* if obeys all rules then find middle of list and accumulate that value*)
-)) true rules)=true then let midval = int_of_string (list_get ((List.length page)/2) page) in acc + midval else acc) 0 pages |> printf "part 1: %d\n";;
+)
+
+(*if pages of the rule dont exist then dont check*)
+let userule update (lp,rp) =
+  not (List.exists (fun x -> x=lp) update) || not (List.exists (fun x -> x=rp) update);;
+
+let part_x update_checker = List.fold_left (fun acc strupdate -> let update = (String.split_on_char ',' strupdate) in 
+  if (List.fold_left (fun acc strrule -> let rule = string_to_rule strrule in (userule update rule) || (update_checker update acc rule)) true rules)=true then let midval = int_of_string (list_get ((List.length update)/2) update) in acc + midval else acc) 0 updates;;
+
+part_x part1 |> printf "part 1: %d\n";;
+
+(* part 2*)
+
+(*
+rules
+
+can we swap positions of two pages without breaking other rules?
+
+a < b
+b < c
+a < c
+ordering a < b < c that is valid
+doing a swap e.g. if b < a then swap to a < b
+b < c is still satisfied
+the a < b < c ordering should hold
+
+right now we need everything as arrays -_-
+*)
+
+let part2 update acc (lp,rp) = acc && (
+  if (List.find_index (fun x -> x=lp) update < List.find_index (fun x -> x=rp) update) then true else (
+    false
+  )
+)
